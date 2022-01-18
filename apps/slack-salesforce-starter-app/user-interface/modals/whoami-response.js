44aac80ecbeb9507748c1bd2ac6a47ac54598001
Garen J. Torikian <gjtorikian@users.noreply.github.com>
2022-01-18 16:53:10 -0500

    Add an exclamation point

--- apps/slack-salesforce-starter-app/user-interface/modals/whoami-response.js
+++ apps/slack-salesforce-starter-app/user-interface/modals/whoami-response.js
@@ -5,7 +5,7 @@ const whoamiresponse = (instanceurl, username) => {
     return Modal({ title: 'Salesforce Slack App', close: 'Close' })
         .blocks(
             Blocks.Section({
-                text: `Successfully connected to salesforce instance ${instanceurl}. Authenticated with user ${username}`
+                text: `Successfully connected to salesforce instance ${instanceurl}. Authenticated with user ${username}!`
             })
         )
         .buildToJSON();
