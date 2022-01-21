872f73a5da468df56cdc1deeb300dcb9effc0a9c
Garen J. Torikian <gjtorikian@users.noreply.github.com>
2022-01-21 15:12:07 -0500

    Add fields

--- apps/slack-salesforce-starter-app/user-interface/app-home/auth-success.js
+++ apps/slack-salesforce-starter-app/user-interface/app-home/auth-success.js
@@ -2,19 +2,24 @@
 
 const { HomeTab, Blocks } = require('slack-block-builder');
 
-const authorization_success_screen = async (username, event_ts, connection) => {
-    const result = await connection.query(
-        `Select Name, Description FROM Contact`
-    );
-    console.log(result);
-    const homeTab = HomeTab({
+const authorization_success_screen = async (username, event_ts, conn) => {
+    const result = await conn.query(`Select Name, Description FROM Contact`);
+    let records = result.records;
+
+    let fields = records.map((record) => {
+        return `*${record.Name}*: ${record.Description}`;
+    });
+
+    const homeTab = await HomeTab({
         callbackId: 'authorization-salesforce-success',
         privateMetaData: 'authenticated'
     }).blocks(
         Blocks.Header({ text: 'Connected to Salesforce' }),
         Blocks.Divider(),
         Blocks.Section({
-            text: `It's ${event_ts}, and you are successfully authenticated to Salesforce as ${username}.`
+            text:
+                'You are successfully authenticated to Salesforce with username ' +
+                username
         })
     );
     return homeTab.buildToJSON();
